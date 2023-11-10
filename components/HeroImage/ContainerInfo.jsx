0aae189e3728ee24a355013bfb4dc3f70fd34796
Leinier Caraballo Yanes <94509566+leiniercy@users.noreply.github.com>
2023-11-09 20:51:20 -0500

    agregue los enlaces a los sitios en la seccion de redes sociales

--- components/HeroImage/ContainerInfo.jsx
+++ components/HeroImage/ContainerInfo.jsx
@@ -17,10 +17,18 @@ const ContainerInfo = () => {
                 <div className="i-make-the">I make the complex simple</div>
             </div>
             <div className="container-social-media">
-                <SocialMedia name={'iconwhatsapp-regular'} icon={'/assets/icon/whatsapp-regular.svg'}/>
-                <SocialMedia name={'telegram'} icon={'/assets/icon/telegram.svg'}/>
-                <SocialMedia name={'facebook-square'} icon={'/assets/icon/facebook-square.svg'}/>
-                <SocialMedia name={'github'} icon={'/assets/icon/github.svg'}/>
+                <a href="https://api.whatsapp.com/send?phone=5358503871" target="_blank">
+                    <SocialMedia name={'iconwhatsapp-regular'} icon={'/assets/icon/whatsapp-regular.svg'}/>
+                </a>
+                <a href="https://t.me/leiniercy" target="_blank">
+                    <SocialMedia name={'telegram'} icon={'/assets/icon/telegram.svg'}/>
+                </a>
+                <a href="https://www.facebook.com/leinier.caraballoyanes/" target="_blank">
+                    <SocialMedia name={'facebook-square'} icon={'/assets/icon/facebook-square.svg'}/>
+                </a>
+                <a href="https://github.com/leiniercy" target="_blank">
+                    <SocialMedia name={'github'} icon={'/assets/icon/github.svg'}/>
+                </a>
             </div>
         </div>);
 };
